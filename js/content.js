a4a9cc4c5cb17bb6be095c52e5a98e1b0fde6a5a
Manasseh <manathe@bitbucket.org>
2018-10-24 06:52:36 +0300

    Enable navigation tool on browser as an extension

--- js/content.js
+++
@@ -1,7 +0,0 @@
-alert("Hello from your Chrome extension!");
-
-var iframe = document.createElement("iframe");  
-iframe.src = "index.html";  
-iframe.width = "800px";  
-//Add the element to the current page
-document.body.appendChild(iframe);
