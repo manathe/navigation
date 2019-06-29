a124621ee1febcabb058f1e102a8d4163fe55567
Manasseh <nassehma@gmail.com>
2019-06-29 06:44:34 +0300

    Change code to use d3 style

--- navigation.js
+++ navigation.js
@@ -1,28 +1,20 @@
 chrome.runtime.sendMessage(
     {'type': 'getRequestedUrls'},
     function generateList(response) {
-      var section = document.querySelector('body>section');
-      var results = response.result;
-      var ol = document.createElement('ol');
-      var li, p, em, code, text;
-      var i;
-      for (i = 0; i < results.length; i++ ) {
-        li = document.createElement('li');
-        p = document.createElement('p');
-        em = document.createElement('em');
-        em.textContent = i + 1;
-        code = document.createElement('code');
-        code.textContent = results[i].url;
-        text = document.createTextNode(
-          chrome.i18n.getMessage('navigationDescription',
-            [results[i].numRequests,
-            results[i].average]));
-        p.appendChild(em);
-        p.appendChild(code);
-        p.appendChild(text);
-        li.appendChild(p);
-        ol.appendChild(li);
-      }
-      section.innerHTML = '';
-      section.appendChild(ol);
+      
+      var section = d3.select("section") //existing section
+
+      var ol = section.append("ol") //lets add our ordered list
+
+      var olJoin = ol.selectAll("ol") // now we need to have a data join
+
+      var olUpdate = olJoin.data(response.result) // we join data to the selection
+
+      //since our selection is empty let's update it
+      var p = olUpdate.enter().append("li").append("p")
+
+      p.append("em").text(function(d, i) { return i + 1 })
+      p.append("code").text(function(d) { return d.url })
+      p.append("text").text(function(d){ return chrome.i18n.getMessage('navigationDescription', [d.numRequests, d.average])})
+
     });
